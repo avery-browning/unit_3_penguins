8567d629492dc526c796388900d66bd8da4a6651
avery-browning <avery.o.browning@gmail.com>
2026-03-03 10:28:32 -0500

    +

--- 2026-03-03_class_script.R
+++ 2026-03-03_class_script.R
@@ -1,2 +1,13 @@
 # 2026-03-03 ; class script
-# Linear models - regression 
+# Linear models - linear regression 
+
+library(palmerpenguins)
+library(tidyverse)
+install.packages("ggiraph")
+library(ggiraph)
+library(GGally)
+install.packages("ggiraphExtra")
+library(ggiraphExtra) # ggPredict()
+library(broom)  # tidy() augment() #does NOT load with tidyverse
+library(car) # vif()
+
